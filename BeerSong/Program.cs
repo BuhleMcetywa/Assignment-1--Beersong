70d7c94c1321c8a72d026b035d7bf6f13bcec213
BuhleMcetywa <buhlemcetywa07@gmail.com>
2023-12-08 11:32:28 +0200

    First commit

---
+++ BeerSong/Program.cs
@@ -0,0 +1,42 @@
+﻿using System.ComponentModel.Design;
+
+namespace BeerSong
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int bottles = 99;
+
+
+            while (bottles > 2)
+            {
+
+
+                Console.WriteLine($"{bottles} bottles of beer on the wall,");
+                Console.WriteLine($"{bottles} bottles of beer,");
+                Console.WriteLine("Take one down, pass it around,");
+
+                bottles--;
+            }
+
+
+            Console.WriteLine($"{bottles} bottles of beer on the wall,");
+            Console.WriteLine($"{bottles} bottles of beer,");
+            Console.WriteLine("Take one down, pass it around,");
+
+            bottles--;
+
+
+            Console.WriteLine($"{bottles} bottle of beer on the wall,");
+            Console.WriteLine($"{bottles} bottle of beer,");
+
+            bottles--;
+
+        }
+    }
+}
+        
+    
+
